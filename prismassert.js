1d8ec250deafa58f5084269df11a55d41bc05e57
garritfra <garritfranke@gmail.com>
2018-05-28 22:04:14 +0200

    isTruthy and isFalsy methods

--- prismassert.js
+++ prismassert.js
@@ -6,4 +6,12 @@ module.exports = class Prismassert {
   static deepEqual(actual, expected) {
     return actual === expected;
   }
+
+  static isTruthy(actual) {
+    return actual == true;
+  }
+
+  static isFalsy(actual) {
+    return actual == false;
+  }
 };
--- test.js
+++ test.js
@@ -9,5 +9,13 @@ assert.equal(prisma.equal(true, "true"), false);
 assert.equal(prisma.deepEqual(1, 1), true);
 assert.equal(prisma.deepEqual(1, "1"), false);
 
+assert.equal(prisma.isTruthy(true), true);
+assert.equal(prisma.isTruthy(false), false);
+assert.equal(prisma.isTruthy(prisma.isFalsy(false)), true);
+
+assert.equal(prisma.isFalsy(false), true);
+assert.equal(prisma.isFalsy(true), false);
+assert.equal(prisma.isFalsy(prisma.isTruthy(false)), true);
+
 console.log("All tests passed");
 return 0;
