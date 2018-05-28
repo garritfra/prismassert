8ea7d3731cf52191d38f74cc5bca7b8adc46b06a
garritfra <garritfranke@gmail.com>
2018-05-28 21:51:59 +0200

    add tests

--- test.js
+++ test.js
@@ -3,6 +3,11 @@ const assert = require("assert");
 
 assert.equal(prisma.equal(1, 1), true);
 assert.equal(prisma.equal(1, 2), false);
-assert.equal(prisma.equal(true, "true"), true);
+assert.equal(prisma.equal(1, "1"), true);
+assert.equal(prisma.equal(true, "true"), false);
+
+assert.equal(prisma.deepEqual(1, 1), true);
+assert.equal(prisma.deepEqual(1, "1"), false);
 
 console.log("All tests passed");
+return 0;
